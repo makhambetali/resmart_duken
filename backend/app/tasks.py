8303e66c3f1038346c7cbf8253590b2499d44f4b
Diyar Makhambetali <dmakhambetali0806@gmail.com>
2025-11-02 16:19:35 +0500

    instructions for celery added

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 @shared_task
 def create_everyday_supply():
     suppliers = Supplier.objects.filter(is_everyday_supply = True)
-    print(f'Найдено {len(suppliers)}: {', '.join([supplier.name for supplier in suppliers])}')
+    # print(f'Найдено {len(suppliers)}: {', '.join([supplier.name for supplier in suppliers])}')
     for supplier in suppliers:
         supply = Supply.objects.create(supplier = supplier, delivery_date = timezone.now().date()+timedelta(days=1))
         print(f'{supply} успешно создан')
