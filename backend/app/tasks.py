552a1ec50380f57eac906b8182db73b6b566ba17
Diyar Makhambetali <dmakhambetali0806@gmail.com>
2025-12-16 02:37:45 +0500

    celery cron task created: shift_supply

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -3,6 +3,7 @@ from app.models import Supply, Supplier
 import time
 from django.utils import timezone
 from datetime import timedelta
+from django.db.models import Q
 
 @shared_task
 def create_everyday_supply():
@@ -13,3 +14,12 @@ def create_everyday_supply():
         print(f'{supply} успешно создан')
     print("Периодическая задача выполняется!")
     return "Задача успешно выполнена."
+
+@shared_task 
+def shift_supply_to_the_next_day():
+    today = timezone.localdate()
+    supplies = Supply.objects.filter(delivery_date = today, is_confirmed=False)
+    supplies.update(
+        delivery_date = today + timedelta(days=1)
+    )
+    
