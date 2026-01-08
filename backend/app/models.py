cda577909e2c5f0f51a9f40ffd5cb21995411503
Diyar Makhambetali <dmakhambetali0806@gmail.com>
2026-01-08 22:14:23 +0500

    improved default admin

--- backend/app/models.py
+++ backend/app/models.py
@@ -100,7 +100,7 @@ class Supply(models.Model):
     store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='supplies', blank=True)
     
     def __str__(self):
-        return f"{self.supplier}: {self.price_bank + self.price_cash}"
+        return f"{self.supplier}|{self.comment}: {self.price_bank + self.price_cash}"
     
     def save(self, *args, **kwargs):
         local_time = timezone.localtime()
